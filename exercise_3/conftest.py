fe6ec085e954862a542e23a215e2cc321a2519c9
inickname <nikolaysmorodinov1@gmail.com>
2025-05-25 18:24:37 +0300

    Добавлены проверки создания и получения элементов

---
+++ exercise_3/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+import requests
+
+from constants import BASE_URL, AUTH_HEADERS, AUTH_DATA, API_HEADERS
+from faker import Faker
+
+fake = Faker()
+
+
+@pytest.fixture(scope="session")
+def auth_session():
+    session = requests.Session()
+    response = session.post(f"{BASE_URL}/api/v1/login/access-token", data=AUTH_DATA, headers=AUTH_HEADERS)
+    assert response.status_code == 200, f"Auth failed: {response.status_code}, {response.text}"
+
+    token = response.json().get("access_token")
+    assert token, "No access_token found"
+
+    session.headers.update(API_HEADERS)
+    session.headers.update({"Authorization": f"Bearer {token}"})
+
+    return session
+
+
+@pytest.fixture()
+def item_data():
+    return {
+        "title": fake.word().capitalize(),
+        "description": fake.sentence(nb_words=10)
+    }
